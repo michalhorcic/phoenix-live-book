7e4b5959ceabb7872f222e36ebfa2574c2d2c4b6
Michal Horcic <michalhorcic@gmail.com>
2021-08-05 15:44:07 +0200

    feat: adding products

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -62,6 +62,13 @@ defmodule PentoWeb.Router do
   scope "/", PentoWeb do
     pipe_through [:browser, :require_authenticated_user]
 
+    live "/products", ProductLive.Index, :index
+    live "/products/new", ProductLive.Index, :new
+    live "/products/:id/edit", ProductLive.Index, :edit
+
+    live "/products/:id", ProductLive.Show, :show
+    live "/products/:id/show/edit", ProductLive.Show, :edit
+
     live "/guess", WrongLive
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
